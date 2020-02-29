b34d4d1d844eb421196668310bd0d7c569cda611
raianoop07 <raianoop07@gmail.com>
2020-02-29 13:15:28 +0530

    first commit

---
+++ limits-service/src/main/java/com/rai/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,30 @@
+package com.rai.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.rai.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
+	}
+}
---
+++ limits-service/src/main/java/com/rai/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,24 @@
+package com.rai.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+	
+	private int maximum;
+	private int minimum;
+	
+	public LimitConfiguration() {
+		super();
+	}
+	
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+}
